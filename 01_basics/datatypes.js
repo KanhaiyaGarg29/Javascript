cf1516c4448adcabc33a6ae7c387041ca39eb7a0
KanhaiyaGarg29 <102350805+KanhaiyaGarg29@users.noreply.github.com>
2024-04-28 09:25:37 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all JS code as newer version
+//number => 2^53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+console.log(typeof "kanhaiya");
+console.log(typeof null); //object
