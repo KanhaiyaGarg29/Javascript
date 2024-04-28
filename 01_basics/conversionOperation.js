69f1e8dea1701963fa66f55d351d2d663fa5efb7
KanhaiyaGarg29 <102350805+KanhaiyaGarg29@users.noreply.github.com>
2024-04-28 09:34:02 +0000

    conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,20 @@
+// let score ="33abc"//NaN
+let score=null; //0
+undefined //NaN
+console.log(typeof(score));
+
+let valueInNumber=Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+/*
+"33" =>33
+"33abc" => NaN
+true => 1;  false => 0
+"" => false
+"kanhaiya" => true
+*/
+
+let a=33;
+let stringNumber=String(a);
+console.log(stringNumber);
